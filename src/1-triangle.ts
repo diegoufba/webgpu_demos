56f8762cbea83b174ef18f33b37d911f9dc5952e
Diego Carapia da Costa <diego.carapia@ufba.br>
2024-05-25 21:03:50 -0300

    adicionado o triagulo

---
+++ src/1-triangle.ts
@@ -0,0 +1,96 @@
+import triangle from './1-triangle.wgsl?raw'
+
+async function main() {
+    const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement
+
+    if (!navigator.gpu) {
+        throw new Error("WebGPU not supported on this browser.")
+    }
+
+    const adpater = await navigator.gpu.requestAdapter()
+    if (!adpater) {
+        throw new Error("No appropriate GPUAdapter found.")
+    }
+
+    const device: GPUDevice = await adpater.requestDevice()
+
+    const context = canvas.getContext('webgpu')
+    if (!context) {
+        throw new Error("WebGPU context not available.");
+    }
+
+    const canvasFormat: GPUTextureFormat = navigator.gpu.getPreferredCanvasFormat()
+    context.configure({
+        device,
+        format: canvasFormat
+    })
+
+
+    const vertices = new Float32Array([
+        -0.8, -0.8,
+        0.8, -0.8,
+        0.8, 0.8,
+    ])
+
+    const vertexBuffer: GPUBuffer = device.createBuffer({
+        label: 'Triangle vertices',
+        size: vertices.byteLength,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    })
+
+    device.queue.writeBuffer(vertexBuffer, 0, vertices)
+
+    const vertexBufferLayout: GPUVertexBufferLayout = {
+        arrayStride: 8,
+        attributes: [{
+            format: "float32x2",
+            offset: 0,
+            shaderLocation: 0,
+        }]
+    }
+
+    const triangleShaderModule: GPUShaderModule = device.createShaderModule({
+        label: 'Triangle shader',
+        code: triangle
+    })
+
+    const trianglePipeline: GPURenderPipeline = device.createRenderPipeline({
+        label: 'Triangle pipeline',
+        layout: 'auto',
+        vertex: {
+            module: triangleShaderModule,
+            entryPoint: "vertexMain",
+            buffers: [vertexBufferLayout]
+        },
+        fragment: {
+            module: triangleShaderModule,
+            entryPoint: 'fragmentMain',
+            targets: [{
+                format: canvasFormat
+            }]
+        }
+    })
+
+    function render() {
+        const encoder: GPUCommandEncoder = device.createCommandEncoder()
+        const textureView: GPUTextureView = context!.getCurrentTexture().createView()
+        const renderPassDescriptor: GPURenderPassDescriptor = {
+            colorAttachments: [{
+                view: textureView,
+                loadOp: 'clear',
+                clearValue: { r: 0.2, g: 0.2, b: 0.298, a: 1 },
+                storeOp: 'store'
+            }]
+        }
+        const pass: GPURenderPassEncoder = encoder.beginRenderPass(renderPassDescriptor)
+        pass.setPipeline(trianglePipeline)
+        pass.setVertexBuffer(0, vertexBuffer)
+        pass.draw(vertices.length / 2)
+        pass.end()
+        device.queue.submit([encoder.finish()])
+    }
+
+    render()
+}
+
+main()
