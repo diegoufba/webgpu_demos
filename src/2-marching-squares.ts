beafed4c9a781f79deb37d9a4558800f9d9c0ca3
Diego Carapia da Costa <diego.carapia@ufba.br>
2024-05-27 13:40:23 -0300

    gui ajustada

--- src/2-marching-squares.ts
+++ src/2-marching-squares.ts
@@ -1,5 +1,6 @@
 import compute from './2-compute.wgsl?raw'
 import shader from './2-shader.wgsl?raw'
+import * as dat from 'dat.gui';
 
 async function main() {
 
@@ -29,14 +30,14 @@ async function main() {
     })
     //*********************************************************************************************************
 
-    let width: number = 512
-    let height: number = 512
+    let width: number = 1024
+    let height: number = 1024
 
     let resolution: number = (width < height) ? width : height // pixels resolution x resolution
-    resolution = 512
+    resolution = width/1.5
     let gridSize: number = Math.floor(resolution / 4) // grid = gridSize x gridSize
     let sideLength: number = resolution / gridSize //square side lenght
-    let shape: number = 3
+    let shape: number = 1
 
     const paramsArrayBuffer = new ArrayBuffer(16) // 2 u32 e 2 f32
     const paramsUint32View = new Uint32Array(paramsArrayBuffer)
@@ -46,28 +47,12 @@ async function main() {
     paramsFloat32View[2] = resolution
     paramsFloat32View[3] = sideLength
 
-    const paramsValues = new Float32Array([resolution,sideLength,gridSize,shape])
     const paramsBuffer: GPUBuffer = device.createBuffer({
         label: 'Params buffer',
         size: paramsArrayBuffer.byteLength,
         usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
     })
-    device.queue.writeBuffer(paramsBuffer,0,paramsArrayBuffer)
-
-    const points = new Float32Array(gridSize * gridSize * 4 * 2)
-
-    const pointsBuffer: GPUBuffer[] = [
-        device.createBuffer({
-            label: 'Points vertices A',
-            size: points.byteLength,
-            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-        }),
-        device.createBuffer({
-            label: 'Points vertices B',
-            size: points.byteLength,
-            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-        })
-    ]
+    device.queue.writeBuffer(paramsBuffer, 0, paramsArrayBuffer)
 
     const bindGroupLayout: GPUBindGroupLayout = device.createBindGroupLayout({
         label: 'Bind Group Layout',
@@ -89,42 +74,65 @@ async function main() {
         ]
     })
 
-    const bindGroup: GPUBindGroup[] = [
-        device.createBindGroup({
-            label: 'Bind Group A',
-            layout: bindGroupLayout,
-            entries: [{
-                binding: 0,
-                resource: { buffer: pointsBuffer[0] },
-            },
-            {
-                binding: 1,
-                resource: { buffer: pointsBuffer[1] },
-            },
-            {
-                binding: 2,
-                resource: { buffer: paramsBuffer },
-            },
-            ]
-        }),
-        device.createBindGroup({
-            label: 'Bind Group B',
-            layout: bindGroupLayout,
-            entries: [{
-                binding: 0,
-                resource: { buffer: pointsBuffer[1] },
-            },
-            {
-                binding: 1,
-                resource: { buffer: pointsBuffer[0] },
-            },
-            {
-                binding: 2,
-                resource: { buffer: paramsBuffer },
-            },
-            ]
-        })
-    ]
+    let pointsBuffer: GPUBuffer[]
+    let bindGroup: GPUBindGroup[]
+    let points: Float32Array
+
+    function createPointsBuffer() {
+        points = new Float32Array(gridSize * gridSize * 4 * 2)
+
+        pointsBuffer = [
+            device.createBuffer({
+                label: 'Points vertices A',
+                size: points.byteLength,
+                usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+            }),
+            device.createBuffer({
+                label: 'Points vertices B',
+                size: points.byteLength,
+                usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+            })
+        ]
+
+        bindGroup = [
+            device.createBindGroup({
+                label: 'Bind Group A',
+                layout: bindGroupLayout,
+                entries: [{
+                    binding: 0,
+                    resource: { buffer: pointsBuffer[0] },
+                },
+                {
+                    binding: 1,
+                    resource: { buffer: pointsBuffer[1] },
+                },
+                {
+                    binding: 2,
+                    resource: { buffer: paramsBuffer },
+                },
+                ]
+            }),
+            device.createBindGroup({
+                label: 'Bind Group B',
+                layout: bindGroupLayout,
+                entries: [{
+                    binding: 0,
+                    resource: { buffer: pointsBuffer[1] },
+                },
+                {
+                    binding: 1,
+                    resource: { buffer: pointsBuffer[0] },
+                },
+                {
+                    binding: 2,
+                    resource: { buffer: paramsBuffer },
+                },
+                ]
+            })
+        ]
+    }
+
+    createPointsBuffer()
 
     const pipelineLayout = device.createPipelineLayout({
         label: 'Pipeline Layout',
@@ -175,8 +183,7 @@ async function main() {
         const computePass = encoder.beginComputePass()
         computePass.setPipeline(simulationPipeline)
         computePass.setBindGroup(0, bindGroup[0])
-        const worgroupCount: number = gridSize
-        computePass.dispatchWorkgroups(worgroupCount / 8, worgroupCount / 8)
+        computePass.dispatchWorkgroups(gridSize / 8, gridSize / 8)
         computePass.end()
 
         const textureView: GPUTextureView = context!.getCurrentTexture().createView()
@@ -198,6 +205,48 @@ async function main() {
     }
 
     render()
+
+    //*********************************************************************************************************
+    // Gui
+    const shapeMap = {
+        star: 1,
+        infinity: 2,
+        circle: 3,
+        heart: 4
+    };
+
+    type Shape = 'star' | 'infinity' | 'circle' | 'heart';
+
+    let gui = new dat.GUI();
+    gui.domElement.style.marginTop = "100px";
+    gui.domElement.id = "datGUI";
+
+    let options = {
+        gridSize: gridSize,
+        shape: 'star' as Shape
+    };
+
+    const shapes: Shape[] = ['star', 'infinity', 'circle', 'heart'];
+
+    gui.add(options, "shape", shapes).onFinishChange((value: Shape) => {
+        shape = shapeMap[value];
+        paramsUint32View[0] = shape
+        device.queue.writeBuffer(paramsBuffer, 0, paramsArrayBuffer)
+        createPointsBuffer()
+        render()
+    });
+
+    gui.add(options, "gridSize", 10, Math.floor(resolution), 10).onFinishChange((value) => {
+        gridSize = value;
+        sideLength = resolution / gridSize;
+        paramsUint32View[1] = gridSize
+        paramsFloat32View[3] = sideLength
+        device.queue.writeBuffer(paramsBuffer, 0, paramsArrayBuffer)
+        createPointsBuffer()
+        render()
+    });
+
+    //*********************************************************************************************************
 }
 
 main()
